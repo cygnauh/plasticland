f50643206e4ed6fcc17a7abe27416b7f9f45df32
cygnauh <cygnauh@gmail.com>
2019-04-08 17:19:47 +0200

    add scene

---
+++ src/components/Scene.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">
+        hello
+    </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+export default {
+    name: 'Scene',
+    data() {
+        return{
+            scene: null
+        }
+    },
+    methods: {
+        test () {
+            this.scene = new THREE.Scene()
+            return 2
+        }
+    }
+}
+</script>
