18ffacc880108275a0ec5bf033fd4a7d086bd969
cygnauh <cygnauh@gmail.com>
2019-04-14 01:17:03 +0200

    :construction: test boat rotation

--- src/three/components/Boat.js
+++ src/three/components/Boat.js
@@ -1,4 +1,4 @@
-// import * as THREE from 'three'
+import * as THREE from 'three'
 import GltfLoaderTest from './GltfLoaderTest'
 
 export default class Boat {
@@ -22,6 +22,44 @@ export default class Boat {
       Math.PI
     )
   }
+  inclinaisonBoat (time) {
+    let points = new Array(3).fill(null) // points around the boat
+    let stepAngle = (Math.PI * 2) / points.length // angle of each point from origin
+    let radius = 1 // cercle radius
+
+    for (let i = 0; i < points.length; i++) {
+      let angle = stepAngle * i
+      points[i] = { // each point positions
+        x: Math.cos(angle) * radius,
+        z: Math.sin(angle) * radius
+      }
+      points[i].y = points[i].x && points[i].z ? this.calculateSurface(points[i].x, points[i].z, time) * 10 : null
+      points[i] = points[i].x && points[i].z ? new THREE.Vector3(points[i].x, points[i].y, points[i].z) : null
+      console.log(points)
+    }
+
+    let cb = new THREE.Vector3()
+    let ab = new THREE.Vector3()
+
+    // var normal = null
+    for (let i = 0; i < points.length; i++) {
+      if (points[2] && points[1]) cb.subVectors(points[2], points[1])
+      if (points[0] && points[1]) ab.subVectors(points[0], points[1])
+      cb.cross(ab) // cross product (produit vectoriel)
+      cb.normalize()
+
+      if (this.object.up) this.object.up.copy(cb) // up vector
+      this.object.matrixNeedsUpdate = true
+
+      let lookAtVector = new THREE.Vector3(0, 0, -1) // camera default lookAt position
+      lookAtVector.applyQuaternion(this.camera.quaternion)
+      lookAtVector.y = 0
+      lookAtVector.add(this.object.position)
+
+      this.object.lookAt(lookAtVector)
+    }
+  }
+
   calculateSurface (x, z, uTime) {
     let y = 0.0
     let SCALE = 10.0
@@ -44,5 +82,6 @@ export default class Boat {
       let y = this.calculateSurface(pos.x, pos.z, time)
       pos.y = y + 1.1
     }
+    this.inclinaisonBoat(time)
   }
 }
