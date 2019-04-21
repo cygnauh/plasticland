4fe2e9ddd639863485e9140e342f4fc6d3cf192c
popo <miss.poepoe23@gmail.com>
2019-04-21 12:21:10 +0200

    :art: cleaning a tiny bit the code

--- src/three/components/Water.js
+++ src/three/components/Water.js
@@ -1,21 +1,22 @@
-import * as THREE from 'three'
+import { Object3D, PlaneBufferGeometry, Mesh, Vector2, Color, TextureLoader, RepeatWrapping, DoubleSide } from 'three';
 import MagicShader from 'magicshader'
 
-export default class Water {
+export default class Water extends Object3D {
     constructor(scene) {
+        super()
         this.scene = scene
 
-        this.initPlane(40, 40, 20, 20)
+        this.initPlane(40, 40 , 20, 20)
         this.addEventListeners()
     }
 
     initPlane(width, height, widthSegments, heightSegments) {
-        this.geometry = new THREE.PlaneBufferGeometry(width, height, widthSegments, heightSegments)
+        this.geometry = new PlaneBufferGeometry(width, height, widthSegments, heightSegments)
         this.geometry.rotateX(-Math.PI / 2);
 
         this.initShader();
 
-        this.plane = new THREE.Mesh(this.geometry,this.material)
+        this.plane = new Mesh(this.geometry,this.material)
         this.plane.position.y = 1;
         this.scene.add(this.plane)
     }
@@ -29,10 +30,12 @@ export default class Water {
             uniform mat4 modelViewMatrix;
             uniform mat4 projectionMatrix;
             
-            #define SCALE 10.0
             varying vec2 vUv;
             uniform float uTime;
             uniform vec2 uKeyboard;
+            
+            const float SCALE = 10.0; 
+            const float STRENGTH = 1.0; 
             
             float calculateSurface(float x, float z) {
                 float y = 0.0;
@@ -45,9 +48,8 @@ export default class Water {
                 vUv = uv;
                 vec3 pos = position;
                 
-                float strength = 1.0;
-                pos.y += strength * calculateSurface(pos.x, pos.z);
-                pos.y -= strength * calculateSurface(0.0, 0.0); // ? 
+                pos.y += STRENGTH * calculateSurface(pos.x, pos.z);
+                // pos.y -= STRENGTH * calculateSurface(0.0, 0.0) ;
             
                 gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
             }  
@@ -56,11 +58,12 @@ export default class Water {
         this.fragmentShader = `
             precision highp float;
         
-            varying vec2 vUv;
+            varying vec2 vUv; 
             uniform sampler2D uMap;
             uniform float uTime;
+            uniform vec2 uKeyboard; 
+            uniform vec2 uMouse; 
             uniform vec3 uColor; // ms({ value: '#ff0000' })
-            uniform vec2 uKeyboard;
             
             void main() {
                 vec2 uv = vUv * 10.0 + vec2(uTime * -0.05);
@@ -70,34 +73,34 @@ export default class Water {
                 uv.y += 0.12 * (sin(uv.x * 4.2 + uTime * 0.64) + sin(uv.x * 6.3 + uTime * 1.65) + sin(uv.x * 8.2 + uTime * 0.45)) / 3.0;
                 
                 vec2 key = uKeyboard;
-                vec4 tex1 = texture2D(uMap, uv * 1.0 + vec2(key.x, key.y));
+                vec2 mouse = uMouse;
+                vec4 tex1 = texture2D(uMap, uv * 1.0 + vec2(key.x, key.y) + vec2(mouse.x, mouse.y)); 
                 vec4 tex2 = texture2D(uMap, uv * 1.0 + vec2(key.x + 0.3, key.y + 0.3));
-            
                 vec3 blue = uColor; 
-            
+          
                 gl_FragColor = vec4(blue + vec3(tex1.a * 0.02 - tex2.a * 0.02), 1.0);
             }
         `;
 
         this.material = new MagicShader({
             uniforms: {
-                uMap: {value: new THREE.TextureLoader().load("https://cinemont.com/tutorials/zelda/water.png", (texture) => {
-                    texture.wrapS = texture.wrapT = THREE.REPEAT_WRAPPING;
+                uMap: {value: new TextureLoader().load("https://cinemont.com/tutorials/zelda/water.png", (texture) => {
+                    texture.wrapS = texture.wrapT = RepeatWrapping;
                 })},
-                uTime: {type: 'f', value: 0},
-                uColor: {type: 'f', value: new THREE.Color('#0051da')},
-                uKeyboard: {type: 'v2', value: new THREE.Vector2(0,0)}
+                uTime: {type: 'f', value: 0.0},
+                uColor: {type: 'f', value: new Color('#d1d3e1')},
+                uKeyboard: {type: 'v2', value: new Vector2(0,0)},
+                uMouse: {type: 'v2', value: new Vector2(0, 0)}
             },
             vertexShader: this.vertexShader,
             fragmentShader: this.fragmentShader,
-            side: THREE.DoubleSide,
+            side: DoubleSide,
             vertexColors: true
         });
     }
 
     addEventListeners() {
         document.addEventListener('keydown', (e) => {
-            const nomTouche = e.key;
             switch (e.key) {
                 case 'ArrowRight':
                     //console.log("turned right")
@@ -120,13 +123,14 @@ export default class Water {
                     console.log(this.plane.material.uniforms.uKeyboard.value.x, this.plane.material.uniforms.uKeyboard.value.y)
                     break
                 default:
-                    console.log(this.plane.material.uniforms.uKeyboard.value.x, this.plane.material.uniforms.uKeyboard.value.y)
                     break
             }
         }, false)
     }
 
-    update(time) {
+    update(time, mouse) {
         this.plane.material.uniforms.uTime.value = time
+        this.plane.material.uniforms.uMouse.x = mouse.x
+        this.plane.material.uniforms.uMouse.y = mouse.y
     }
 }
