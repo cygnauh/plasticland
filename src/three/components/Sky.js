a569647d292f3e9946026b287a87e2bda1b4092c
cygnauh <cygnauh@gmail.com>
2019-05-03 09:53:00 +0200

    lint warning

--- src/three/components/Sky.js
+++ src/three/components/Sky.js
@@ -17,31 +17,29 @@
 import * as THREE from 'three'
 
 THREE.Sky = function () {
+  var shader = THREE.Sky.SkyShader
 
-  var shader = THREE.Sky.SkyShader;
-
-  var material = new THREE.ShaderMaterial( {
+  var material = new THREE.ShaderMaterial({
     fragmentShader: shader.fragmentShader,
     vertexShader: shader.vertexShader,
-    uniforms: THREE.UniformsUtils.clone( shader.uniforms ),
+    uniforms: THREE.UniformsUtils.clone(shader.uniforms),
     side: THREE.BackSide
-  } );
+  })
 
-  THREE.Mesh.call( this, new THREE.BoxBufferGeometry( 1, 1, 1 ), material );
+  THREE.Mesh.call(this, new THREE.BoxBufferGeometry(1, 1, 1), material)
+}
 
-};
-
-THREE.Sky.prototype = Object.create( THREE.Mesh.prototype );
+THREE.Sky.prototype = Object.create(THREE.Mesh.prototype)
 
 THREE.Sky.SkyShader = {
 
   uniforms: {
-    "luminance": { value: 1 },
-    "turbidity": { value: 2 },
-    "rayleigh": { value: 1 },
-    "mieCoefficient": { value: 0.005 },
-    "mieDirectionalG": { value: 0.8 },
-    "sunPosition": { value: new THREE.Vector3() }
+    'luminance': { value: 1 },
+    'turbidity': { value: 2 },
+    'rayleigh': { value: 1 },
+    'mieCoefficient': { value: 0.005 },
+    'mieDirectionalG': { value: 0.8 },
+    'sunPosition': { value: new THREE.Vector3() }
   },
 
   vertexShader: [
@@ -83,40 +81,40 @@ THREE.Sky.SkyShader = {
     'const float EE = 1000.0;',
 
     'float sunIntensity( float zenithAngleCos ) {',
-    '	zenithAngleCos = clamp( zenithAngleCos, -1.0, 1.0 );',
-    '	return EE * max( 0.0, 1.0 - pow( e, -( ( cutoffAngle - acos( zenithAngleCos ) ) / steepness ) ) );',
+    ' zenithAngleCos = clamp( zenithAngleCos, -1.0, 1.0 );',
+    ' return EE * max( 0.0, 1.0 - pow( e, -( ( cutoffAngle - acos( zenithAngleCos ) ) / steepness ) ) );',
     '}',
 
     'vec3 totalMie( float T ) {',
-    '	float c = ( 0.2 * T ) * 10E-18;',
-    '	return 0.434 * c * MieConst;',
+    ' float c = ( 0.2 * T ) * 10E-18;',
+    ' return 0.434 * c * MieConst;',
     '}',
 
     'void main() {',
 
-    '	vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
-    '	vWorldPosition = worldPosition.xyz;',
+    ' vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
+    ' vWorldPosition = worldPosition.xyz;',
 
-    '	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-    '	gl_Position.z = gl_Position.w;', // set z to camera.far
+    ' gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+    ' gl_Position.z = gl_Position.w;', // set z to camera.far
 
-    '	vSunDirection = normalize( sunPosition );',
+    ' vSunDirection = normalize( sunPosition );',
 
-    '	vSunE = sunIntensity( dot( vSunDirection, up ) );',
+    ' vSunE = sunIntensity( dot( vSunDirection, up ) );',
 
-    '	vSunfade = 1.0 - clamp( 1.0 - exp( ( sunPosition.y / 450000.0 ) ), 0.0, 1.0 );',
+    ' vSunfade = 1.0 - clamp( 1.0 - exp( ( sunPosition.y / 450000.0 ) ), 0.0, 1.0 );',
 
-    '	float rayleighCoefficient = rayleigh - ( 1.0 * ( 1.0 - vSunfade ) );',
+    ' float rayleighCoefficient = rayleigh - ( 1.0 * ( 1.0 - vSunfade ) );',
 
     // extinction (absorbtion + out scattering)
     // rayleigh coefficients
-    '	vBetaR = totalRayleigh * rayleighCoefficient;',
+    ' vBetaR = totalRayleigh * rayleighCoefficient;',
 
     // mie coefficients
-    '	vBetaM = totalMie( turbidity ) * mieCoefficient;',
+    ' vBetaM = totalMie( turbidity ) * mieCoefficient;',
 
     '}'
-  ].join( '\n' ),
+  ].join('\n'),
 
   fragmentShader: [
     'varying vec3 vWorldPosition;',
@@ -150,13 +148,13 @@ THREE.Sky.SkyShader = {
     'const float ONE_OVER_FOURPI = 0.07957747154594767;',
 
     'float rayleighPhase( float cosTheta ) {',
-    '	return THREE_OVER_SIXTEENPI * ( 1.0 + pow( cosTheta, 2.0 ) );',
+    ' return THREE_OVER_SIXTEENPI * ( 1.0 + pow( cosTheta, 2.0 ) );',
     '}',
 
     'float hgPhase( float cosTheta, float g ) {',
-    '	float g2 = pow( g, 2.0 );',
-    '	float inverse = 1.0 / pow( 1.0 - 2.0 * g * cosTheta + g2, 1.5 );',
-    '	return ONE_OVER_FOURPI * ( ( 1.0 - g2 ) * inverse );',
+    ' float g2 = pow( g, 2.0 );',
+    ' float inverse = 1.0 / pow( 1.0 - 2.0 * g * cosTheta + g2, 1.5 );',
+    ' return ONE_OVER_FOURPI * ( ( 1.0 - g2 ) * inverse );',
     '}',
 
     // Filmic ToneMapping http://filmicgames.com/archives/75
@@ -170,54 +168,53 @@ THREE.Sky.SkyShader = {
     'const float whiteScale = 1.0748724675633854;', // 1.0 / Uncharted2Tonemap(1000.0)
 
     'vec3 Uncharted2Tonemap( vec3 x ) {',
-    '	return ( ( x * ( A * x + C * B ) + D * E ) / ( x * ( A * x + B ) + D * F ) ) - E / F;',
+    ' return ( ( x * ( A * x + C * B ) + D * E ) / ( x * ( A * x + B ) + D * F ) ) - E / F;',
     '}',
-
 
     'void main() {',
     // optical length
     // cutoff angle at 90 to avoid singularity in next formula.
-    '	float zenithAngle = acos( max( 0.0, dot( up, normalize( vWorldPosition - cameraPos ) ) ) );',
-    '	float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - ( ( zenithAngle * 180.0 ) / pi ), -1.253 ) );',
-    '	float sR = rayleighZenithLength * inverse;',
-    '	float sM = mieZenithLength * inverse;',
+    ' float zenithAngle = acos( max( 0.0, dot( up, normalize( vWorldPosition - cameraPos ) ) ) );',
+    ' float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - ( ( zenithAngle * 180.0 ) / pi ), -1.253 ) );',
+    ' float sR = rayleighZenithLength * inverse;',
+    ' float sM = mieZenithLength * inverse;',
 
     // combined extinction factor
-    '	vec3 Fex = exp( -( vBetaR * sR + vBetaM * sM ) );',
+    ' vec3 Fex = exp( -( vBetaR * sR + vBetaM * sM ) );',
 
     // in scattering
-    '	float cosTheta = dot( normalize( vWorldPosition - cameraPos ), vSunDirection );',
+    ' float cosTheta = dot( normalize( vWorldPosition - cameraPos ), vSunDirection );',
 
-    '	float rPhase = rayleighPhase( cosTheta * 0.5 + 0.5 );',
-    '	vec3 betaRTheta = vBetaR * rPhase;',
+    ' float rPhase = rayleighPhase( cosTheta * 0.5 + 0.5 );',
+    ' vec3 betaRTheta = vBetaR * rPhase;',
 
-    '	float mPhase = hgPhase( cosTheta, mieDirectionalG );',
-    '	vec3 betaMTheta = vBetaM * mPhase;',
+    ' float mPhase = hgPhase( cosTheta, mieDirectionalG );',
+    ' vec3 betaMTheta = vBetaM * mPhase;',
 
-    '	vec3 Lin = pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * ( 1.0 - Fex ), vec3( 1.5 ) );',
-    '	Lin *= mix( vec3( 1.0 ), pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * Fex, vec3( 1.0 / 2.0 ) ), clamp( pow( 1.0 - dot( up, vSunDirection ), 5.0 ), 0.0, 1.0 ) );',
+    ' vec3 Lin = pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * ( 1.0 - Fex ), vec3( 1.5 ) );',
+    ' Lin *= mix( vec3( 1.0 ), pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * Fex, vec3( 1.0 / 2.0 ) ), clamp( pow( 1.0 - dot( up, vSunDirection ), 5.0 ), 0.0, 1.0 ) );',
 
     // nightsky
-    '	vec3 direction = normalize( vWorldPosition - cameraPos );',
-    '	float theta = acos( direction.y ); // elevation --> y-axis, [-pi/2, pi/2]',
-    '	float phi = atan( direction.z, direction.x ); // azimuth --> x-axis [-pi/2, pi/2]',
-    '	vec2 uv = vec2( phi, theta ) / vec2( 2.0 * pi, pi ) + vec2( 0.5, 0.0 );',
-    '	vec3 L0 = vec3( 0.1 ) * Fex;',
+    ' vec3 direction = normalize( vWorldPosition - cameraPos );',
+    ' float theta = acos( direction.y ); // elevation --> y-axis, [-pi/2, pi/2]',
+    ' float phi = atan( direction.z, direction.x ); // azimuth --> x-axis [-pi/2, pi/2]',
+    ' vec2 uv = vec2( phi, theta ) / vec2( 2.0 * pi, pi ) + vec2( 0.5, 0.0 );',
+    ' vec3 L0 = vec3( 0.1 ) * Fex;',
 
     // composition + solar disc
-    '	float sundisk = smoothstep( sunAngularDiameterCos, sunAngularDiameterCos + 0.00002, cosTheta );',
-    '	L0 += ( vSunE * 19000.0 * Fex ) * sundisk;',
+    ' float sundisk = smoothstep( sunAngularDiameterCos, sunAngularDiameterCos + 0.00002, cosTheta );',
+    ' L0 += ( vSunE * 19000.0 * Fex ) * sundisk;',
 
-    '	vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );',
+    ' vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );',
 
-    '	vec3 curr = Uncharted2Tonemap( ( log2( 2.0 / pow( luminance, 4.0 ) ) ) * texColor );',
-    '	vec3 color = curr * whiteScale;',
+    ' vec3 curr = Uncharted2Tonemap( ( log2( 2.0 / pow( luminance, 4.0 ) ) ) * texColor );',
+    ' vec3 color = curr * whiteScale;',
 
-    '	vec3 retColor = pow( color, vec3( 1.0 / ( 1.2 + ( 1.2 * vSunfade ) ) ) );',
+    ' vec3 retColor = pow( color, vec3( 1.0 / ( 1.2 + ( 1.2 * vSunfade ) ) ) );',
 
-    '	gl_FragColor = vec4( retColor, 1.0 );',
+    ' gl_FragColor = vec4( retColor, 1.0 );',
 
     '}'
-  ].join( '\n' )
+  ].join('\n')
 
-};
+}
