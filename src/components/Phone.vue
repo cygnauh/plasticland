56b4279fb14f9da35f42017f46a22ad48d23b044
cygnauh <cygnauh@gmail.com>
2019-05-14 15:57:58 +0200

    improve photo

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -1,28 +1,59 @@
 <template>
-  <div class="Phone">
-      <div class="Phone-bg">
-          <div class="Phone-border">
-              <div class="countdown-container">
-                  <div class="countdown">
-                      <span>{{ count }}</span>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+    <div :class="{'show-phone': showPhone}"
+         class="Phone">
+        <div
+            :class="{show: showPhoto}"
+            class="Phone-bg">
+            <div
+                class="Phone-dark">
+                <div
+                    :class="{open: openCamera}"
+                    class="dark up">
+                </div>
+                <div
+                    :class="{open: openCamera}"
+                    class="dark down">
+                </div>
+            </div>
+            <div class="Phone-border">
+                <div
+                    v-if="openCamera && !showPhoto"
+                    class="countdown-container">
+                    <div class="countdown">
+                        <span>{{ count }}</span>
+                    </div>
+                </div>
+                <img
+                    v-if="showPhoto"
+                    :src="require('../assets/img/svg/close.svg')"
+                    alt="close">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'Phone',
+  props: {
+    title: {
+      type: String
+    }
+  },
   data () {
     return {
+      showPhone: false,
+      openCamera: false,
       count: 3,
-	  countDown: null
+      countDown: null,
+      showPhoto: false
     }
   },
   mounted () {
     this.countToZero()
+    setTimeout(() => { // this.openCamera = true
+	  this.showPhone = true
+    }, 1000)
   },
   methods: {
     countToZero () {
@@ -35,6 +66,7 @@ export default {
       }, 1000)
     },
     stopCountDown () {
+      this.showPhoto = true
       clearInterval(this.countDown)
     }
   }
@@ -48,20 +80,62 @@ export default {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translateX(-50%) translateY(-50%);
+    will-change: transform;
+    transform: translateX(-1300px) translateY(500px) rotate(-50deg);
+    transition: transform 1s ease-out;
+    &.show-phone{
+        transform: translateX(-50%) translateY(-50%) rotate(0deg);
+    }
     &-bg {
         border-radius: 45px;
         height: calc(100% - 4px);
-        background: url('../assets/img/photograph_plastic_ocean.jpg') no-repeat;
-        background-position-y: 4px;
-        background-size: 100% 100%;
+        z-index: 1;
+        /*position: absolute;*/
+        &.show{
+            background: url('../assets/img/photograph_plastic_ocean.jpg') no-repeat;
+            background-position-y: 4px;
+            background-size: 100% 100%;
+        }
+        .Phone-dark{
+            position: absolute;
+            z-index: 0;
+            border-radius: 45px;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            width: 100%;
+            /*background: rgba(10, 10, 10, 0.5);*/
+            overflow: hidden;
+            .dark{
+                height: 50%;
+                width: 100%;
+                /*position: absolute;*/
+                background: black;
+                border: solid 1px red;
+                z-index: 2;
+                will-change: transform;
+                transition: transform 0.5s ease;
+                &.open.up{
+                    transform: translateY(-100%);
+                }
+                &.open.down{
+                    transform: translateY(100%);
+                }
+            }
+        }
     }
     &-border{
+        position: relative;
         height: 100%;
         background: url('../assets/img/svg/phone-border.svg') no-repeat;
         background-size: cover;
         display: flex;
         justify-content: center;
+        z-index: 3;
+        img{
+            position: relative;
+            top: 80px;
+        }
         .countdown-container{
             border: solid 1px $sand_yellow;
             width: 150px;
