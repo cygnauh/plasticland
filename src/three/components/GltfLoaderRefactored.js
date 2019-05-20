2fc7b49250478ab1e8344ccbb418fe66dd07ef39
popo <miss.poepoe23@gmail.com>
2019-05-20 20:40:26 +0200

    GltfLoader: remove unused code

--- src/three/components/GltfLoaderRefactored.js
+++ src/three/components/GltfLoaderRefactored.js
@@ -2,28 +2,34 @@ import * as THREE from 'three'
 import GLTFLoader from '../utils/GLTFLoader'
 
 export default class GltfLoaderRefactored {
-  constructor (name, path, scene, manager, { posX = 0, posY = 0, posZ = 0, scale = 1, found = true, addToScene = true, rotateX = 0, rotateY = 0, rotateZ = 0 }) {
+  constructor (name, path, scene, manager, {
+    posX = 0,
+    posY = 0,
+    posZ = 0,
+    scale = 1,
+    rotateX = 0,
+    rotateY = 0,
+    rotateZ = 0,
+    found = true,
+    addToScene = true
+  }) {
     this.scene = scene
     this.name = name
     this.loader = new THREE.GLTFLoader(manager)
     this.gltf = null
     let gltfChild = null
-	  let geometries = []
+    let geometries = []
     let materials = []
-	  let meshes = []
-
-    // draco loader
-    // see gltf-pipeline
+    let meshes = []
 
     let promise = new Promise((resolve, reject) => {
       this.loader.load(path, (gltf) => {
-        this.gltf = gltf.scene // TODO fix and replace, gltf.scene.children[0] to get de meshes
+        this.gltf = gltf.scene
         this.gltf.name = name
         // this.gltf.castShadow = true
-        // this.scene.scale.multiplyScalar(scale)
         this.gltf.traverse(function (child) {
           if (child.isMesh) {
-	          gltfChild = child
+            gltfChild = child
             child.position.x = posX
             child.position.y = posY
             child.position.z = posZ
