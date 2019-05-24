8fd46422ec1d208d8eda7944aef2327b7297660b
popo <miss.poepoe23@gmail.com>
2019-05-24 12:05:30 +0200

    eslint issue

--- src/data/photographs.js
+++ src/data/photographs.js
@@ -1,28 +1,28 @@
 export default {
-	'photographs': [
-		{
-			'id': 10,
-			'name': 'photo1',
-			'url': './models/starbucks_cup.glb',
-			'open': false
-		},
-		{
-			'id': 20,
-			'name': 'photo2',
-			'url': './models/starbucks_cup.glb',
-			'open': false
-		},
-		{
-			'id': 30,
-			'name': 'photo3',
-			'url': './models/starbucks_cup.glb',
-			'open': false
-		},
-		{
-			'id': 40,
-			'name': 'photo4',
-			'url': './models/starbucks_cup.glb',
-			'open': true
-		}
-	]
+  'photographs': [
+    {
+      'id': 10,
+      'name': 'photo1',
+      'url': './models/starbucks_cup.glb',
+      'open': false
+    },
+    {
+      'id': 20,
+      'name': 'photo2',
+      'url': './models/starbucks_cup.glb',
+      'open': false
+    },
+    {
+      'id': 30,
+      'name': 'photo3',
+      'url': './models/starbucks_cup.glb',
+      'open': false
+    },
+    {
+      'id': 40,
+      'name': 'photo4',
+      'url': './models/starbucks_cup.glb',
+      'open': true
+    }
+  ]
 }
