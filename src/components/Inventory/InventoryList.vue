79ca134ab098e143dd80b021b9090829f440c5a0
cygnauh <cygnauh@gmail.com>
2019-05-26 21:27:50 +0200

    inventory inte update

--- src/components/Inventory/InventoryList.vue
+++ src/components/Inventory/InventoryList.vue
@@ -9,14 +9,35 @@
           :id="object.name"
           :class="{'canHover' : object.found}"
           :key="object.id"
-          v-for="(object, i) in objects"
-          class="object"
+          class="el first-object obj"
+          v-for="(object) in objects.slice(0, 1)"
           @click.capture="(e) => onObjectClicked(e, object)">
+          <div class="number"> {{object.id}} </div>
+        </div>
+        <div class="el grow-three">
           <div
-            :class=" (i+1) % 3 ? 'border-right': null"
-            class="obj-container">
-            <div class="obj-title">
-              {{ object.title }}
+            class="container">
+            <div
+              :ref="object.name"
+              :id="object.name"
+              :class="[{'canHover' : object.found}, {'grow-two': (i === 1)}]"
+              :key="object.id"
+              class="obj"
+              v-for="(object, i) in objects.slice(1, 3)"
+              @click.capture="(e) => onObjectClicked(e,object)">
+              <div class="number">{{object.id}}</div>
+            </div>
+          </div>
+          <div class="container">
+            <div
+              :key="object.id"
+              :ref="object.name"
+              :id="object.name"
+              :class="{'canHover' : object.found}"
+              @click.capture="(e) => onObjectClicked(e, object)"
+              v-for="(object) in objects.slice(3)"
+              class="obj">
+              <div class="number">{{object.id}}</div>
             </div>
           </div>
         </div>
@@ -86,73 +107,55 @@ export default {
         align-items: flex-start;
         flex-direction: row;
         transform: translateY(159px);
-        .object{
-          border: solid 1px yellow;
-          transform: translateZ(0);
-          width: 33.0%;
-          &:before{
-            content: '';
-            position: absolute;
-            z-index: -1;
-            left: 0;
-            right: 100%;
-            top: 0;
-            background-color: $medium_grey;
-            height: 1px;
-            transition-property: right;
-            transition-duration: 0.3s;
-            transition-timing-function: ease-out;
-          }
-          a{
-            text-decoration: none;
-          }
-          .obj-container{
-            will-change: opacity;
-            padding: 20px;
-            height: 35vh;
-            display: flex;
-            background: none;
+        flex-flow: row wrap;
+        width: calc(100% - 40px);
+        height: 73vh;
+        padding: 20px;
+        .canHover{
+          .obj-container:hover{
+            background: url('../../assets/img/svg/border-collection-list-element.svg') center no-repeat;
+            background-size: 90% 90%;
             .obj-title{
-              text-decoration: none;
-              will-change: opacity;
-              opacity: 0;
-              height: auto;
-              width: 100%;
-              align-self: flex-end;
-              font-size: 18px;
-              font-family: Arkhip, sans-serif;
-              color: $sand_yellow;
-              text-transform: uppercase;
-              padding-bottom: 10px;
-              margin: 10px;
+              opacity: 1;
             }
           }
-          &.canHover{
-            .obj-container:hover{
-              background: url('../../assets/img/svg/border-collection-list-element.svg') center no-repeat;
-              background-size: 90% 90%;
-              .obj-title{
-                opacity: 1;
-              }
-            }
+        }
+        .el {
+          height: 100%;
+          &.first-object{
+            margin-left: 5px;
           }
-          .border-right{
-            transform: translateZ(0);
-            &:before{
-              content: '';
-              position: absolute;
-              z-index: -1;
-              right: 0;
-              top: 0;
-              background-color: $medium_grey;
-              height: 0;
-              width: 1px;
-              transition: height 0.3s ease-out;
-              transition-delay: 0.6s;
-            }
-            &:hover:before{
+          &.grow-three{
+            flex-grow: 3;
+            display: flex;
+            flex-direction: column;
+            .container{
+              display: flex;
+              flex: 1 0 0;
               height: 100%;
             }
+          }
+          flex: 1 0 0;
+          display: flex;
+          flex-direction: row;
+          /*margin: 5px 5px 5px 0;*/
+        }
+        .obj{
+          flex: 1 0 0;
+          border: solid 1px $sand_yellow;
+          border-radius: 8px;
+          margin: 0 5px 5px 0;
+          height: 100%;
+          &.grow-two{
+            flex-grow: 2;
+          }
+          .number{
+            width: 39px;
+            height: 39px;
+            background: $sand_yellow;
+            color: white;
+            text-align: center;
+            border-radius: 8px 2px 2px 2px;
           }
         }
       }
