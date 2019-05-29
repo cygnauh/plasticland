fa2d2a6ca45a1706c77e790935ede6cdd01c5b7b
cygnauh <cygnauh@gmail.com>
2019-05-30 01:13:16 +0200

    update inventory data

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -4,48 +4,48 @@ export default
   'objects': [
     {
       'id': 1,
-      'name': 'plastique1',
-      'title': 'prendre la vie côté plastique 1',
+      'name': 'starbucks',
+      'title': 'CE N\'EST PAS JUSTE DU PLASTIQUE, C\'EST STARBUCKS',
       'model': './models/starbucks_cup.glb',
       'found': false,
-      'description': 'Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70.Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70.'
+      'description': 'La plupart des gobelets à boissons recouverts de carton sont difficilement recyclables du fait d\'une fine couche de plastique qui borde la face interne. C\'est le cas pour cette célèbre marque de café qui a reconnu les faits en 2008. Pour se racheter, elle a organisé un grand concours pour rede­si­gner ses gobelets et enfin respecter comme il se doit l’en­vi­ron­n­ement. Après cinq années de compétition, elle a pris la décision d’an­nu­ler le concours et de continuer à utiliser ses gobelets polluants, en attendant que les techniques de recyclage s’adaptent à ses gobelets. Aux dernières nouvelles, la situation reste inchangée et chaque année dans le monde, quatre milliards de gobelets non-recyclables atterrissent dans l\'environnement.'
     },
     {
       'id': 2,
-	    'name': 'plastique2',
-	    'title': 'prendre la vie côté plastique 2',
+	    'name': 'carrefour',
+	    'title': 'LES PRIX BAS, LE SUREMBALLAGE EN PLUS.',
       'model': './models/bottle_coca.glb',
       'found': true,
-      'description': 'Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70.Consigne arrêter chez Coca cola dans les 70.@ Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70.'
+	    'description': 'Des tomates coupées en deux et vendues dans des barquettes recouvertes d’un film plastique, des bananes emballées individuellement dans de petits sachets en plastique. Ce sont des images que dénoncent les internautes. Dans leur viseur, les hypermarchés qui participent largement à la multiplication de ces produits.@L’autre raison évoquée principalement par la plupart des Français, c’est le suremballage des produits Bio dont les emballages ne sont pas forcément nécessaires ce qui les rends autant nocifs pour l’environnement que les pesticides.\n'
     },
     {
       'id': 3,
-      'name': 'p3',
-	    'title': 'prendre la vie côté plastique 3',
+      'name': 'cocacola',
+	    'title': 'PRENDS LA VIE COTE PLASTIQUE\n',
       'model': './models/starbucks_cup_test.glb',
       'found': false,
-      'description': 'Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70.'
+      'description': 'Dans les années 50, plusieurs marques de soda utilisaient encore les bouteilles en verre pour la distribution de leur produit. Des bouteilles réutilisable grâce au système de consigne mise en place par les marques.@Mais à partir de 1960, l’industrialisation et l’apparition de la matière plastique ouvrent de nouvelles perspectives pour les firmes qui décident d\'abandonner la bouteille en verre consigné.@Une décision qui ne prend pas en compte les rapports des scientifiques qui alertaient déjà sur les dangers du plastique à usage unique.'
     },
     {
       'id': 4,
-      'name': 'p4',
-	    'title': 'prendre la vie côté plastique 4',
+      'name': 'gestespropres',
+	    'title': 'MES PLASTIQUES, TU RECYCLERAS',
       'model': './models/starbucks_cup_test.glb',
       'found': false,
-      'description': 'Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70. @Consigne arrêter chez Coca cola dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez Coca cola dans les 70.'
+      'description': 'Après avoir verdi leur image via de vastes campagnes de communication sur le recyclage, les industriels ont adopté une nouvelle stratégie : celle de culpabiliser le consommateur à travers le financement des OGM dont l’objet est d’organiser de grands évènements de ramassage de déchets dans la nature et dont le mot d’ordre consiste à pointer « l’incivilité » individuelle.@En finançant ces ONG, les industriels déplacent le débat sur d\'autres questions autre que l\'abandon de l\'utilisation du plastique. Ils peuvent ainsi continuer à produire pendant que les utilisateurs s\'efforcent à recycler.'
     },
     {
       'id': 5,
-      'name': 'p5',
-	    'title': 'prendre la vie côté plastique 5',
+      'name': 'nestle',
+	    'title': 'TU POUSSES LE BOUCHON UN PEU TROP LOIN NESTLE',
       'model': './models/bottle_coca.glb',
       'found': false,
-      'description': 'Consigne arrêter dans les 70. @Consigne arrêter dans les 70. Consigne arrêter chez dans les 70.@ Consigne arrêter dans les 70.@ Consigne arrêter chez dans les 70. @Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70.@ Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez star dans les 70.'
+      'description': 'Au cours des cinq dern... coming soon'
     },
     {
       'id': 6,
-      'name': 'p6',
-	    'title': 'Ce n\'est pas juste du plastique, c\'est starbucks.',
+      'name': 'final',
+	    'title': 'le grand final in progress',
       'model': './models/starbucks_cup.glb',
       'found': true,
       'description': 'Consigne arrêter dans les 70 Consigne arrêter dans les 70. Consigne arrêter chez dans les 70.Consigne arrêter dans les 70 Consigne arrêter chez dans les 70. @Consigne arrêter chez star dans les 70. Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70.@ Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez star dans les 70.'
