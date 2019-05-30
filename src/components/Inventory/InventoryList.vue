f8c5922030190d32e37fd74778ad416876cbeb9d
cygnauh <cygnauh@gmail.com>
2019-05-30 17:36:14 +0200

    inventory inte

--- src/components/Inventory/InventoryList.vue
+++ src/components/Inventory/InventoryList.vue
@@ -5,42 +5,67 @@
     <div class='inventory-container'>
       <div id="box" class="box">
         <div
-          :ref="object.name"
-          :id="object.name"
-          :class="{'canHover' : object.found}"
           :key="object.id"
+          :class="{'canHover' : object.found}"
           class="el first-object obj"
           v-for="(object) in objects.slice(0, 1)"
           @click.capture="(e) => onObjectClicked(e, object)">
-          <div class="number"> {{object.id}} </div>
-          <div class="title"> {{ object.title }} </div>
+          <!--<img-->
+            <!--:src="require('../../assets/img/svg/grow-vertical-y.svg')"-->
+            <!--alt="border">-->
+          <div
+            :ref="object.name"
+            :id="object.name"
+            class="border"
+          >
+            <div
+              :class="{'y-bg' : object.found}"
+              class="number"> {{object.id}} </div>
+            <div class="title"> {{ object.title }} </div>
+          </div>
         </div>
         <div class="el grow-three">
           <div
             class="container">
             <div
-              :ref="object.name"
-              :id="object.name"
-              :class="[{'canHover' : object.found}, {'grow-two': (i === 1)}]"
               :key="object.id"
+              :class="[{'canHover' : object.found}, {'grow-two': (i === 1)}]"
               class="obj"
               v-for="(object, i) in objects.slice(1, 3)"
               @click.capture="(e) => onObjectClicked(e,object)">
-              <div class="number">{{object.id}}</div>
-              <div class="title"> {{ object.title }} </div>
+              <!--<img-->
+                <!--:src="[ i === 1 && object.found ? require('../../assets/img/svg/grow-two-y.svg') : i === 1 && !object.found ? require('../../assets/img/svg/grow-two-g.svg') : i === 0 && object.found ? require('../../assets/img/svg/grow-one-y.svg') : require('../../assets/img/svg/grow-one-y.svg') ]"-->
+                <!--alt="border">-->
+              <div
+                :ref="object.name"
+                :id="object.name"
+                class="border">
+                <div
+                  :class="{'y-bg' : object.found}"
+                  class="number">{{object.id}}</div>
+                <div class="title"> {{ object.title }} </div>
+              </div>
             </div>
           </div>
           <div class="container">
             <div
               :key="object.id"
-              :ref="object.name"
-              :id="object.name"
               :class="{'canHover' : object.found}"
               @click.capture="(e) => onObjectClicked(e, object)"
               v-for="(object) in objects.slice(3)"
               class="obj">
-              <div class="number">{{object.id}}</div>
-              <div class="title"> {{ object.title }} </div>
+              <!--<img-->
+                <!--:src="[ object.found ? require('../../assets/img/svg/grow-one-y.svg') : require('../../assets/img/svg/grow-one-g.svg') ]"-->
+                <!--alt="border">-->
+              <div
+                :ref="object.name"
+                :id="object.name"
+                class="border">
+                <div
+                  :class="{'y-bg' : object.found}"
+                  class="number">{{object.id}}</div>
+                <div class="title"> {{ object.title }} </div>
+              </div>
             </div>
           </div>
         </div>
@@ -119,6 +144,8 @@ export default {
           height: 100%;
           &.first-object{
             margin-left: 5px;
+            /*background: url('../../assets/img/svg/grow-vertical-y.svg') no-repeat;*/
+            /*background-size: cover;*/
           }
           &.grow-three{
             flex-grow: 3;
@@ -137,33 +164,74 @@ export default {
         }
         .obj{
           flex: 1 0 0;
-          border: solid 4px $sand_yellow;
-          border-radius: 8px;
-          margin: 0 5px 5px 0;
+          /*padding: 15px;*/
           height: 100%;
           position: relative;
           font-family: AxeHandel, sans-serif;
+          .border{
+            /*margin: 15px 15px 15px 0;*/
+            border: solid 4px $light_blue;
+            border-radius: 5px;
+            height: calc(100% - 15px);
+            width: calc(100% - 15px);
+          }
+          /*background: url('../../assets/img/svg/grow-one-g.svg') no-repeat center center;*/
+          /*background-size: cover;*/
           &.grow-two{
             flex-grow: 2;
+            /*background: url('../../assets/img/svg/grow-two-g.svg') no-repeat center center;*/
+            /*background-size: cover;*/
+            &.canHover {
+              /*background: url('../../assets/img/svg/grow-two-y.svg') no-repeat center center;*/
+              /*background-size: cover;*/
+            }
           }
           .number{
-            width: 42px;
-            height: 42px;
-            background: $sand_yellow;
+            width: 45px;
+            height: 45px;
             color: white;
             text-align: center;
-            border-radius: 8px 2px 2px 2px;
+            /*border-radius: 8px 2px 2px 2px;*/
             font-size: 41px;
             font-weight: bold;
+            position: relative;
+            left: -1px;
+            top: -1px;
+            background: url('../../assets/img/svg/grey-bg.svg') center center;
+            background-size: contain;
+            will-change: background;
+            transition: background 0.1s ease-in-out;
           }
-          &.canHover{
+          .title{
             will-change: opacity;
             opacity: 1; // TODO to 0 in prod
+            transition: opacity 0.2s ease-in-out;
+          }
+          &.canHover{
+            /*background: url('../../assets/img/svg/grow-one-y.svg') no-repeat center center;*/
+            /*background-size: cover;*/
+            &:hover{
+              .border{
+                border: solid 4px $sand_yellow;
+              }
+              .y-bg{
+                background: url('../../assets/img/svg/yellow-bg.svg') center center;
+                background-size: contain;
+              }
+            }
+
             &:hover{
               .title{
                 opacity: 1;
               }
             }
+          }
+          img{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
           }
           .title{
             color: $sand_yellow;
