b6b57e85848786ead186e1452e245f6c40167fb7
popo <miss.poepoe23@gmail.com>
2019-06-02 23:56:52 +0200

    :bento: Radar: added map

--- src/components/Radar/Radar.vue
+++ src/components/Radar/Radar.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="radar">
         <div class="radar-background">
-            <!--<div class="radar-map"></div>-->
-            <div >
-                <div>
-                    <div class="radar-circle-el yellow center"></div>
-                    <div class="radar-circle-el dotted small center"></div>
-                    <div class="radar-circle-el dotted medium center"></div>
-                </div>
-                <div class="center">
-                    <div class="radar-cross-el left"></div>
-                    <div class="radar-cross-el right"></div>
-                </div>
-                <div class="radar-boat center"></div>
-                <div class="radar-line">
-                    <div class="radar-line-el"></div>
-                </div>
+            <div class="radar-map center" ></div>
+            <div>
+                <div class="radar-circle-el yellow center"></div>
+                <div class="radar-circle-el dotted small center"></div>
+                <div class="radar-circle-el dotted medium center"></div>
+            </div>
+            <div class="center">
+                <div class="radar-cross-el left"></div>
+                <div class="radar-cross-el right"></div>
+            </div>
+            <div class="radar-boat center"></div>
+            <div class="radar-line">
+                <div class="radar-line-el"></div>
             </div>
         </div>
     </div>
