bc5760a662c3560e4a43448e8502d26b0e7f25b5
cygnauh <cygnauh@gmail.com>
2019-06-07 01:50:27 +0200

    update sound data

--- src/data/sounds.js
+++ src/data/sounds.js
@@ -8,63 +8,63 @@ export default {
     {
       'id': 21,
       'name': 'intro1',
-      'src': './sounds/test1.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.02,
       'endAt': 0.07
     },
     {
       'id': 21,
       'name': 'intro2',
-      'src': './sounds/test1.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.08,
       'endAt': 0.13
     },
     {
       'id': 21,
       'name': 'intro2bis',
-      'src': './sounds/test1.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.14,
       'endAt': 0.23
     },
     {
       'id': 21,
       'name': 'starbucks',
-      'src': './sounds/test1.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.24,
       'endAt': 0.459
     },
     {
       'id': 22,
       'name': 'carrefour',
-      'src': './sounds/test2.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.46,
       'endAt': 0.61
     },
     {
       'id': 23,
       'name': 'cocacola',
-      'src': './sounds/sound_ambiant_xp.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.62,
       'endAt': 0.70
     },
     {
       'id': 24,
       'name': 'gestespropres',
-      'src': './sounds/test1.mp3',
+      'src': './sounds/test1.carrefour',
       'startAt': 0.71,
       'endAt': 0.79
     },
     {
       'id': 25,
       'name': 'nestle',
-      'src': './sounds/test2.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.8,
       'endAt': 0.86
     },
     {
       'id': 26,
       'name': 'final',
-      'src': './sounds/sound_ambiant_xp.mp3',
+      'src': './sounds/carrefour.mp3',
       'startAt': 0.9,
       'endAt': 1
     }
