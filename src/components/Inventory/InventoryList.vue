ca999b73c7e8911a9b65ca29e95dcaca35bdb40e
popo <miss.poepoe23@gmail.com>
2019-06-10 15:04:24 +0200

    Add grain to inventory (css-trick)

--- src/components/Inventory/InventoryList.vue
+++ src/components/Inventory/InventoryList.vue
@@ -119,9 +119,22 @@ export default {
     /*overflow: scroll;*/
     z-index: 1;
     top: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     will-change: transform;
+    &:after {
+      content: "";
+      animation: grain 8s steps(10) infinite;
+      background-image: url('../../assets/img/grain.jpg');
+      position: fixed;
+      height: 300%;
+      width: 300%;
+      top: -100%;
+      left: -50%;
+      opacity: 0.3;
+      mix-blend-mode: hue;
+      z-index: -1;
+    }
     &.show{
       transition: transform 1s ease-in-out;
     }
@@ -290,5 +303,18 @@ export default {
         }
       }
     }
+  }
+
+  @keyframes grain {
+    0%, 100% { transform:translate(0, 0) }
+    10% { transform:translate(-5%, -10%) }
+    20% { transform:translate(-15%, 5%) }
+    30% { transform:translate(7%, -25%) }
+    40% { transform:translate(-5%, 25%) }
+    50% { transform:translate(-15%, 10%) }
+    60% { transform:translate(15%, 0%) }
+    70% { transform:translate(0%, 15%) }
+    80% { transform:translate(3%, 35%) }
+    90% { transform:translate(-10%, 10%) }
   }
 </style>
