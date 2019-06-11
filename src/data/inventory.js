4c7c6a19e2ee816c2fb1c4c680047074f57d4854
cygnauh <cygnauh@gmail.com>
2019-06-11 02:23:57 +0200

    add value for interaction sound

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -12,6 +12,8 @@ export default
       'soundSrc': './sounds/starbucks.mp3',
       'found': false,
       'position': 0.26,
+      'interactionSound': 'interactionSt',
+      'ruptureSoundAt': 92.01,
       'description': 'La plupart des gobelets à boissons recouverts de carton sont difficilement recyclables du fait d\'une fine couche de plastique qui borde la face interne. C\'est le cas pour cette célèbre marque de café qui a reconnu les faits en 2008. Pour se racheter, elle a organisé un grand concours pour rede­si­gner ses gobelets et enfin respecter comme il se doit l’en­vi­ron­n­ement. Après cinq années de compétition, elle a pris la décision d’an­nu­ler le concours et de continuer à utiliser ses gobelets polluants, en attendant que les techniques de recyclage s’adaptent à ses gobelets. Aux dernières nouvelles, la situation reste inchangée et chaque année dans le monde, quatre milliards de gobelets non-recyclables atterrissent dans l\'environnement.'
     },
     {
@@ -24,6 +26,8 @@ export default
       'soundSrc': './sounds/carrefour.mp3',
       'found': false,
       'position': 0.38,
+      'interactionSound': 'interactionCr',
+      'ruptureSoundAt': 101.19,
       'description': 'Des tomates coupées en deux et vendues dans des barquettes recouvertes d’un film plastique, des bananes emballées individuellement dans de petits sachets en plastique. Ce sont des images que dénoncent les internautes. Dans leur viseur, les hypermarchés qui participent largement à la multiplication de ces produits.@L’autre raison évoquée principalement par la plupart des Français, c’est le suremballage des produits Bio dont les emballages ne sont pas forcément nécessaires ce qui les rends autant nocifs pour l’environnement que les pesticides.\n'
     },
     {
@@ -36,6 +40,8 @@ export default
       'soundSrc': './sounds/cocacola.mp3',
       'found': false,
       'position': 0.565,
+      'interactionSound': 'interactionCc',
+      'ruptureSoundAt': 107.2,
       'description': 'Dans les années 50, plusieurs marques de soda utilisaient encore les bouteilles en verre pour la distribution de leur produit. Des bouteilles réutilisable grâce au système de consigne mise en place par les marques.@Mais à partir de 1960, l’industrialisation et l’apparition de la matière plastique ouvrent de nouvelles perspectives pour les firmes qui décident d\'abandonner la bouteille en verre consigné.@Une décision qui ne prend pas en compte les rapports des scientifiques qui alertaient déjà sur les dangers du plastique à usage unique.'
     },
     {
@@ -48,6 +54,8 @@ export default
       'title': 'MES PLASTIQUES, TU RECYCLERAS',
       'found': false,
       'position': 0.68,
+      'interactionSound': 'interactionLb',
+      'ruptureSoundAt': 114.12,
       'description': 'Après avoir verdi leur image via de vastes campagnes de communication sur le recyclage, les industriels ont adopté une nouvelle stratégie : celle de culpabiliser le consommateur à travers le financement des OGM dont l’objet est d’organiser de grands évènements de ramassage de déchets dans la nature et dont le mot d’ordre consiste à pointer « l’incivilité » individuelle.@En finançant ces ONG, les industriels déplacent le débat sur d\'autres questions autre que l\'abandon de l\'utilisation du plastique. Ils peuvent ainsi continuer à produire pendant que les utilisateurs s\'efforcent à recycler.'
     },
     {
@@ -60,6 +68,8 @@ export default
       'soundSrc': './sounds/lobbies.mp3',
       'found': false,
       'position': 0.90,
+      'interactionSound': 'interactionNe',
+      'ruptureSoundAt': 119.23,
       'description': 'Au cours des cinq dern... coming soon'
     },
     {
@@ -72,6 +82,7 @@ export default
       'soundSrc': './sounds/lobbies.mp3',
       'found': false,
       'position': 1,
+      'interactionSound': '',
       'description': 'Consigne arrêter dans les 70 Consigne arrêter dans les 70. Consigne arrêter chez dans les 70.Consigne arrêter dans les 70 Consigne arrêter chez dans les 70. @Consigne arrêter chez star dans les 70. Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70Consigne arrêter chez star dans les 70 Consigne arrêter chez star dans les 70.@ Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.@Consigne arrêter chez star dans les 70. @Consigne arrêter chez star dans les 70. C’est la mort de la bouteille en verre en faveur de la bouteille de plastique.Consigne arrêter chez star dans les 70.'
     }
   ]
