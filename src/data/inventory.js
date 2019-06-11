6206c06b5a996f26512f9b25731a903e2d470f4c
cygnauh <cygnauh@gmail.com>
2019-06-11 23:45:46 +0200

    fix

--- src/data/inventory.js
+++ src/data/inventory.js
@@ -11,7 +11,7 @@ export default
       'model': './models_converted/starbucks_cup.glb',
       'soundSrc': './sounds/starbucks.mp3',
       'found': false,
-      'position': 0.26,
+      'position': 0.285,
       'interactionSound': 'interactionSt',
       'ruptureSoundAt': 92.01,
       'description': 'La plupart des gobelets à boissons recouverts de carton sont difficilement recyclables du fait d\'une fine couche de plastique qui borde la face interne. C\'est le cas pour cette célèbre marque de café qui a reconnu les faits en 2008. Pour se racheter, elle a organisé un grand concours pour rede­si­gner ses gobelets et enfin respecter comme il se doit l’en­vi­ron­n­ement. Après cinq années de compétition, elle a pris la décision d’an­nu­ler le concours et de continuer à utiliser ses gobelets polluants, en attendant que les techniques de recyclage s’adaptent à ses gobelets. Aux dernières nouvelles, la situation reste inchangée et chaque année dans le monde, quatre milliards de gobelets non-recyclables atterrissent dans l\'environnement.'
@@ -25,7 +25,7 @@ export default
       'model': './models_converted/barquette_04.glb',
       'soundSrc': './sounds/carrefour.mp3',
       'found': false,
-      'position': 0.38,
+      'position': 0.41,
       'interactionSound': 'interactionCr',
       'ruptureSoundAt': 101.19,
       'description': 'Des tomates coupées en deux et vendues dans des barquettes recouvertes d’un film plastique, des bananes emballées individuellement dans de petits sachets en plastique. Ce sont des images que dénoncent les internautes. Dans leur viseur, les hypermarchés qui participent largement à la multiplication de ces produits.@L’autre raison évoquée principalement par la plupart des Français, c’est le suremballage des produits Bio dont les emballages ne sont pas forcément nécessaires ce qui les rends autant nocifs pour l’environnement que les pesticides.\n'
@@ -39,7 +39,7 @@ export default
       'model': './models_converted/bottle_coca.glb',
       'soundSrc': './sounds/cocacola.mp3',
       'found': false,
-      'position': 0.565,
+      'position': 0.60,
       'interactionSound': 'interactionCc',
       'ruptureSoundAt': 107.2,
       'description': 'Dans les années 50, plusieurs marques de soda utilisaient encore les bouteilles en verre pour la distribution de leur produit. Des bouteilles réutilisable grâce au système de consigne mise en place par les marques.@Mais à partir de 1960, l’industrialisation et l’apparition de la matière plastique ouvrent de nouvelles perspectives pour les firmes qui décident d\'abandonner la bouteille en verre consigné.@Une décision qui ne prend pas en compte les rapports des scientifiques qui alertaient déjà sur les dangers du plastique à usage unique.'
@@ -53,21 +53,21 @@ export default
       'soundSrc': './sounds/lobbies.mp3',
       'title': 'MES PLASTIQUES, TU RECYCLERAS',
       'found': false,
-      'position': 0.68,
+      'position': 0.71,
       'interactionSound': 'interactionLb',
       'ruptureSoundAt': 114.12,
       'description': 'Après avoir verdi leur image via de vastes campagnes de communication sur le recyclage, les industriels ont adopté une nouvelle stratégie : celle de culpabiliser le consommateur à travers le financement des OGM dont l’objet est d’organiser de grands évènements de ramassage de déchets dans la nature et dont le mot d’ordre consiste à pointer « l’incivilité » individuelle.@En finançant ces ONG, les industriels déplacent le débat sur d\'autres questions autre que l\'abandon de l\'utilisation du plastique. Ils peuvent ainsi continuer à produire pendant que les utilisateurs s\'efforcent à recycler.'
     },
     {
       'id': 5,
-      'x': -236.435,
-      'z': -979.22,
+      'x': -235.54,
+      'z': -1000,
       'name': 'nestle',
       'title': 'TU POUSSES LE BOUCHON UN PEU TROP LOIN NESTLE',
       'model': './models_converted/trophee_nestle.glb',
       'soundSrc': './sounds/lobbies.mp3',
       'found': false,
-      'position': 0.90,
+      'position': 0.93,
       'interactionSound': 'interactionNe',
       'ruptureSoundAt': 119.23,
       'description': 'Au cours des cinq dernières années, NESTLE, c\'est 1,7 million de tonnes de plastique produits par an avec une augmentation de 5% dans le portefeuille d\'utilisation d\'emballage plastique. Aujourd\'hui près de 98 % des produits Nestlé sont vendus dans des emballages à usage unique. Pas étonnant qu\'il se retrouve en bonne place du top 10 des entreprises les plus responsables de la pollution plastique. Un classement réalisé en 2018 par la coalition Break Free Plastic dans le cadre d\'une vaste opération de collecte dans 42 pays à travers le monde et la participation de 10 000 volontaires. \n'
